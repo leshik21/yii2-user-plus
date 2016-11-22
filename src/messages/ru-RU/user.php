fb7dd043aaca3e3766664954b0a9c1fc6711517a
leshik21 <lesh1g@yandex.ru>
2016-11-22 12:20:53 +0300

    Messages for russian language added

--- src/messages/ru-RU/user.php
+++ src/messages/ru-RU/user.php
@@ -1,2 +1,8 @@
 <?php
-return [];
+return [
+    'Sign in' => 'Войти',
+    'Password' => 'Пароль',
+    'Email' => 'E-mail',
+    'Remember me next time' => 'Запомнить',
+    'Invalid login or password' => 'Неправильный email или пароль',
+];
